08c8657add5ff74f8cc298d46e29229f5e2e747e
kyle <kyle.goines@gmail.com>
2016-05-04 15:25:02 -0400

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
